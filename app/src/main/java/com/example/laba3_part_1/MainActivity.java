e9c68619ad9ae06247a202d917f04757f0f427ec
mikha <mikhail.p2005@gmail.com>
2024-11-06 16:36:02 +0300

    kuku

---
+++ app/src/main/java/com/example/laba3_part_1/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.laba3_part_1;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseHelper dbHelper;
+    private SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbHelper = new DatabaseHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        // Инициализируем базу данных примерными данными
+        dbHelper.clearAndInsertSampleData(db);
+
+        // Настройка кнопок
+        Button buttonView = findViewById(R.id.button2);
+        Button buttonAdd = findViewById(R.id.button);
+        Button buttonUpdate = findViewById(R.id.button3);
+
+        buttonView.setOnClickListener(v -> openViewActivity());
+        buttonAdd.setOnClickListener(v -> addNewStudent());
+        buttonUpdate.setOnClickListener(v -> updateLastStudent());
+    }
+
+    private void openViewActivity() {
+        Intent intent = new Intent(MainActivity.this, ViewActivity.class);
+        startActivity(intent);
+    }
+
+    private void addNewStudent() {
+        try {
+            db.execSQL("INSERT INTO " + DatabaseHelper.TABLE_NAME + " (" + DatabaseHelper.COLUMN_FIO + ") VALUES ('Новый Студент')");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateLastStudent() {
+        try {
+            db.execSQL("UPDATE " + DatabaseHelper.TABLE_NAME + " SET " + DatabaseHelper.COLUMN_FIO +
+                    " = 'Иванов Иван Иванович' WHERE " + DatabaseHelper.COLUMN_ID +
+                    " = (SELECT MAX(" + DatabaseHelper.COLUMN_ID + ") FROM " + DatabaseHelper.TABLE_NAME + ")");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (db != null && db.isOpen()) {
+            db.close();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/laba3_part_1/ViewActivity.java
@@ -0,0 +1,63 @@
+package com.example.laba3_part_1;
+
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ViewActivity extends AppCompatActivity {
+    DatabaseHelper dbHelper;
+    SQLiteDatabase db;
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> studentList;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        dbHelper = new DatabaseHelper(this);
+        db = dbHelper.getReadableDatabase();
+        listView = findViewById(R.id.listView);
+
+        studentList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, studentList);
+        listView.setAdapter(adapter);
+
+        loadRecords();
+    }
+
+    private void loadRecords() {
+        try (Cursor cursor = db.rawQuery("SELECT * FROM " + DatabaseHelper.TABLE_NAME, null)) {
+            if (cursor.moveToFirst()) {
+                do {
+                    @SuppressLint("Range")
+                    String fio = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_FIO));
+                    @SuppressLint("Range")
+                    String addedTime = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_TIME));
+                    studentList.add(fio + " - " + addedTime);
+                } while (cursor.moveToNext());
+            }
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (db != null && db.isOpen()) {
+            db.close();
+        }
+        if (dbHelper != null) {
+            dbHelper.close();
+        }
+    }
+}
